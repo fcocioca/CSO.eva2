2d8f3ba6d151532bb504649705c753574e3bda10
Francisco Cocio <francisco.cocio.cancino@ciisa.cl>
2022-07-30 17:01:45 -0400

    2Carga funcional desarrollo con rutas controller

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\rutasController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,26 +13,30 @@ use Illuminate\Support\Facades\Route;
 |
 */
 //rutas formato directo
-Route::get('/', function () {
-    return view('index');
-});
-Route::get('/index', function () {
-    return view('index');
-});
-Route::get('/listarProducto', function () {
-    return view('listarProducto');
-});
-Route::get('/ingresaP', function () {
-    return view('ingresaP');
-});
-Route::get('/eliminaP', function () {
-    return view('eliminaP');
-});
-Route::get('/bodega/{sucursal?}/{direccion?}', function($sucursal="Mi Bodega", $direccion="Stgo Centro"){
-    return view('bodega')
-    ->with('sucursal',$sucursal);
-});
-
-
+// Route::get('/', function () {
+//     return view('index');
+// });
+// Route::get('/index', function () {
+//     return view('index');
+// });
+// Route::get('/listarProducto', function () {
+//     return view('listarProducto');
+// });
+// Route::get('/ingresaP', function () {
+//     return view('ingresaP');
+// });
+// Route::get('/eliminaP', function () {
+//     return view('eliminaP');
+// });
+// Route::get('/bodega/{sucursal?}/{direccion?}', function($sucursal="Mi Bodega", $direccion="Stgo Centro"){
+//     return view('bodega')
+//     ->with('sucursal',$sucursal);
+// });
 
 //rutas via controller
+Route::get('/index', [rutasController::class, 'showindex']);
+Route::get('/', [rutasController::class, 'showindex']);
+Route::get('/listarProducto', [rutasController::class, 'showlistarproducto']);
+Route::get('/bodega/{sucursal?}/{direccion?}', [rutasController::class, 'showbodega']);
+Route::get('/ingresaP', [rutasController::class, 'showIngresaP']);
+Route::get('/eliminaP', [rutasController::class, 'showeliminaP']);
