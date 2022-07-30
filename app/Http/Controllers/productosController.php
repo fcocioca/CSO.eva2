2021e95587f4ed54e0f65916c318fac8f8966e8e
Francisco Cocio <francisco.cocio.cancino@ciisa.cl>
2022-07-30 17:34:10 -0400

    5Carga funcional con migraciones

---
+++ app/Http/Controllers/productosController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class productosController extends Controller
+{
+    
+    public function agregarProducto (Request $request) {
+        // dd($request);
+        $this->validate($request,[
+            'txtCodigo' => 'required',
+            'txtNombre' => 'required |min:3',
+            'txtValor' => 'required | numeric',
+            'slctCategoria' => 'required',
+            'txtCantidad' => 'required | numeric',
+        
+        ]);
+        return "Producto: ". $request->input('txtNombre');
+    }
+}
---
+++ app/Http/Controllers/rutasController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class rutasController extends Controller
+{
+    public function showindex () {
+        return view('/index');      
+    }
+
+    public function showeliminaP () {
+        return view('/eliminaP');        
+    }
+    public function showIngresaP () {
+        return view('/ingresaP');        
+    }
+    public function showbodega () {
+        return view('/bodega')
+        ->with('sucursal',"Mi Bodega");       
+    }
+    public function showlistarproducto () {
+        return view('/listarProducto')
+        ->with('arrayProducto',array('1','2','3','4','5'));     
+    }
+}
